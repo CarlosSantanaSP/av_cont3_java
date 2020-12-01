a5b23c8fefcb84d32b10248ad57c2345d6ad2d39
CarlosSantanaSP <carlos.santana@bandtec.com.br>
2020-12-01 03:13:35 -0300

    Correção

--- installer_av_cont3_java.sh
+++ installer_av_cont3_java.sh
@@ -2,27 +2,28 @@
 
 sudo cp avaliacao-continuada3/target/avaliacao-continuada3-1.0-SNAPSHOT-jar-with-dependencies.jar ../../
 
-which java | grep –q /bin/java
+which java | grep –q /usr/bin/java
 
 echo $?
 
 if [ $? = 0 ]
     then
-        
-        echo "Executando .jar"        
+        echo "Instalando java"
+
+        sudo apt install default-jdk
 
         sudo chmod +x avaliacao-continuada3/target/avaliacao-continuada3-1.0-SNAPSHOT-jar-with-dependencies.jar
 
         sudo java -jar avaliacao-continuada3/target/avaliacao-continuada3-1.0-SNAPSHOT-jar-with-dependencies.jar
 
-
     else
-        echo "Instalando java"                
+        echo "Executando .jar"
 
-        sudo apt install default-jdk
-        
-        sudo chmod +x avaliacao-continuada3/target/avaliacao-continuada3-1.0-SNAPSHOT-jar-with-dependencies.jar 
+        sudo chmod +x avaliacao-continuada3/target/avaliacao-continuada3-1.0-SNAPSHOT-jar-with-dependencies.jar
 
         sudo java -jar avaliacao-continuada3/target/avaliacao-continuada3-1.0-SNAPSHOT-jar-with-dependencies.jar
+
 fi
+
+
 
