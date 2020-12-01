6a489a1f56f0b225cc6687cbf453562656b54f96
CarlosSantanaSP <carlos.santana@bandtec.com.br>
2020-12-01 00:32:40 -0300

    Finalização

--- installer_av_cont3_java.sh
+++ installer_av_cont3_java.sh
@@ -1,53 +1,34 @@
 #!/bin/bash
 
-cd $HOME
+#cd $HOME
+        
 
-ls | grep -q av_cont3_java
+which java | grep –q /usr/bin/java
 
 if [ $? = 0 ]
     then
-        echo "Clonando repositório"
+        echo "Instalando SDKman"                
 
-        git clone "https://github.com/CarlosSantanaSP/av_cont3_java.git"
+        sudo apt install zip
 
-        mkdir caixa_eletronico
+        curl -s "http://get.sdkman.io" | bash
 
-        cp av_cont3_java/avaliacao-continuada3/target/avaliacao-continuada3-1.0-SNAPSHOT-jar-with-dependencies.jar $HOME/Desktop/caixa_eletronico/
-        cp av_cont3_java/installer_av_cont3_java.sh $HOME/Desktop/caixa_eletronico/
-        cp av_cont3_java/README.txt $HOME/Desktop/caixa_eletronico
+        SOURCE "$HOME/.sdkman/bin/sdkman-init.sh"
 
-        cd $HOME/Desktop/
+        echo "Instalando java 11.0.9.open-adpt"
 
-        which java | grep –q /usr/bin/java
-
-        if [ $? = 0 ]
-            then
-                echo "Instalando SDKman"                
-
-                sudo apt install zip
-
-                curl -s "http://get.sdkman.io" | bash
-
-                SOURCE "$HOME/.sdkman/bin/sdkman-init.sh"
-
-                echo "Instalando java 11.0.9.open-adpt"
-
-                sdk install java 11.0.9.open-adpt
-
-            else
-                echo "Executando .jar"        
-
-                chmod +x avaliacao-continuada3-1.0-SNAPSHOT-jar-with-dependencies.jar 
-
-                java -jar avaliacao-continuada3-1.0-SNAPSHOT-jar-with-dependencies.jar
-        fi
-
-    else 
-        echo "atualiza repo"
-
-        cd $HOME/Desktop/av_cont3_java/
-
-        git pull
+        sdk install java 11.0.9.open-adpt
         
-        java -jar avaliacao-continuada3-1.0-SNAPSHOT-jar-with-dependencies.jar
+        chmod +x avaliacao-continuada3/target/avaliacao-continuada3-1.0-SNAPSHOT-jar-with-dependencies.jar 
+
+        java -jar avaliacao-continuada3/target/avaliacao-continuada3-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+    else
+        echo "Executando .jar"        
+
+        chmod +x avaliacao-continuada3/target/avaliacao-continuada3-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+
+        java -jar avaliacao-continuada3/target/avaliacao-continuada3-1.0-SNAPSHOT-jar-with-dependencies.jar
 fi
+
